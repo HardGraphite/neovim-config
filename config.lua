199833ebe61858a29767db28953ec3008b1d5d12
HardGraphite <jodieydchen@gmail.com>
2023-11-05 18:10:57 +0800

    Adjust Neovim options

--- config.lua
+++ config.lua
@@ -22,6 +22,15 @@ usepkg.now("onedark", {
 vim_o.number = true
 vim_o.relativenumber = true
 
+--- marks and rulers ---
+vim_o.hlsearch = true
+vim_o.cursorline = true
+vim_o.colorcolumn = 80
+vim_o.list = true
+vim_o.listchars = "tab:▸ ,trail:·"
+vim_o.showcmd = false
+vim_o.showmode = false
+
 --- status line ---
 usepkg.now("lualine", {
   options = {
@@ -31,3 +40,35 @@ usepkg.now("lualine", {
     globalstatus = true,
   }
 })
+
+
+-------------------------------------------------
+--------------| Editor behaviour |---------------
+-------------------------------------------------
+
+--- records and backups ---
+vim_o.history = 64
+vim_o.backup = false
+vim_o.undofile = false
+vim_o.swapfile = false
+
+--- file formats ---
+vim_o.fileformats = "unix"
+vim_o.encoding = "utf-8"
+
+--- text display ---
+vim_o.wrap = true
+vim_o.scrolloff = 5
+
+--- indentations ---
+vim_o.autoindent = true
+vim_o.smartindent = true
+vim_o.tabstop = 8
+vim_o.softtabstop = 4
+vim_o.shiftwidth = 4
+vim_o.expandtab = true
+
+--- search ---
+vim_o.incsearch = true
+vim_o.smartcase = true
+vim_o.ignorecase = true
